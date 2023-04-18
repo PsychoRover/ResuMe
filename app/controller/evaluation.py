07863b01fd00abeef3b2e613dfa46cbf6fec3b1c
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-04-18 18:15:11 +0300

    Delete app directory

--- app/controller/evaluation.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile
-
-router = APIRouter(prefix="/v1")
-
-
-@router.post("/upload")
-async def upload_cv(file: UploadFile):
-    print(file.filename)
-    return {"filename": file.read()}
