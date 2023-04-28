9108abd5db3061369e5d565a46c3a1c0e035c6d0
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-04-28 10:19:18 +0300

    Add Logger to `main.py`

--- app/model_service/main.py
+++ app/model_service/main.py
@@ -2,6 +2,7 @@ import typing
 
 import tensorflow as tf
 from fastapi import FastAPI
+from loguru import logger
 
 from app.model_service.functions import get_classes
 
@@ -10,13 +11,16 @@ if typing.TYPE_CHECKING:
     from numpy import ndarray
 
 classes = get_classes("./classes.toml")
+
+logger.info(" ---- Loading Model ----")
 model: "Model" = tf.keras.models.load_model("./model")
+logger.info(" ---- Done Loading Model ----")
 
 app = FastAPI()
 
 
 @app.post("/predict")
-def predict(cv: list[str]) -> tuple[float, str]:
+def predict(cv: list[str]) -> typing.Tuple[float, str]:
     raw_prediction: "ndarray" = model.predict(cv).squeeze()
     prediction = max(zip(raw_prediction, classes))
     return prediction
