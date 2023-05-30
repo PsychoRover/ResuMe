6c0e21b7d4de3a05fbab364d7c89215f7c1ecbd4
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-05-30 20:48:06 +0300

    Add `ENDPOINT` Constant

--- app/main_service/constants.py
+++ app/main_service/constants.py
@@ -1,6 +1,7 @@
 MAIN_SERVICE = "app/main_service"
+ENDPOINT = "https://d33c-35-186-149-171.ngrok.io/"
 
 
 class Folders:
-    TEMPLATES = f"{MAIN_SERVICE}/templates"
-    STATIC = f"{MAIN_SERVICE}/static"
+    TEMPLATES = f"templates"
+    STATIC = f"static"
