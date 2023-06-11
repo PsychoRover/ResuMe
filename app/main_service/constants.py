8426a49ba61cc32afa84ca680955b34c824fea42
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-06-11 18:08:38 +0000

    Add Skill Extraction With OpenAI

--- app/main_service/constants.py
+++ app/main_service/constants.py
@@ -1,7 +1,17 @@
 MAIN_SERVICE = "app/main_service"
-ENDPOINT = "https://faef-34-143-137-250.ngrok.io/predict"
+ENDPOINT = "https://7e96-34-142-178-226.ngrok.io/predict"
 
 
 class Folders:
-    TEMPLATES = f"templates"
-    STATIC = f"static"
+    TEMPLATES = f"{MAIN_SERVICE}/templates"
+    STATIC = f"{MAIN_SERVICE}/static"
+
+
+chat_prompt = lambda category, cv_text: (
+    f"Take the following Resume and extract list of the skills that are for this category: {category}:\n"
+    f"{cv_text}\n"
+    f"and another list of recommended 5 skills for this profession that are not in the first list or in the resume at all.\n"
+    f"Return answer in the following format:\n"
+    f"[skill1, skill2, skill3, skill4, skill5], [missing_skill1, missing_skill2, missing_skill3, missing_skill4, missing_skill5]\n"
+    f"each list and each [] can have up to 5 skills (or less)."
+)
--- app/main_service/controllers/prediction.py
+++ app/main_service/controllers/prediction.py
@@ -1,13 +1,14 @@
 import requests
+from fastapi import APIRouter, File, Request, UploadFile
 from PyPDF2.errors import PdfReadError
-from fastapi import APIRouter, UploadFile, File, Request
 from starlette.templating import Jinja2Templates
 
-from app.main_service.functions import parse_pdf
-from constants import ENDPOINT
+from ..constants import ENDPOINT, Folders, chat_prompt
+from ..functions.openai import extract_skills, llm
+from ..functions.pdf import parse_pdf
 
 router = APIRouter(prefix="/v1")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=Folders.TEMPLATES)
 
 
 @router.post("/upload")
@@ -16,12 +17,26 @@ async def upload_pdf(request: Request, pdf_file: UploadFile = File(...)):
         cv_text, name, email = parse_pdf(pdf_file.file)
     except PdfReadError:
         return "Wrong file extension, please upload .PDF or .DOCX"
+
     response = requests.post(
         ENDPOINT,
         json=[cv_text],
     )
     assurance, category = response.json()
-    return templates.TemplateResponse("prediction.html",
-                                      {"request": request, "prediction": category, "assurance": f"{assurance:.2f}",
-                                       "name": name,
-                                       "email": email})
+
+    model_response = llm.predict(chat_prompt(category, cv_text))
+
+    relevant_skills, missing_skills = extract_skills(model_response)
+
+    return templates.TemplateResponse(
+        "prediction.html",
+        {
+            "request": request,
+            "prediction": category,
+            "assurance": f"{assurance:.2f}",
+            "name": name,
+            "email": email,
+            "relevant_skills": relevant_skills,
+            "missing_skills": missing_skills,
+        },
+    )
