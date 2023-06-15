9ee98344844b068a647f062b7f3b1765af5a9577
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-06-15 18:20:23 +0000

    Add New Logic For Recruiters Analysis

--- app/main_service/controllers/homepage.py
+++ app/main_service/controllers/homepage.py
@@ -1,25 +1,21 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 
-from app.main_service.constants import Folders
+from ..templates.templates import About, Analyzer, Homepage
 
 router = APIRouter()
-
-# ------- Templates ------- #
-templates = Jinja2Templates(directory=Folders.TEMPLATES)
 
 
 @router.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
-    return templates.TemplateResponse("homepage.html", {"request": request})
+    return Homepage(context={"request": request})
 
 
 @router.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request})
+    return About(context={"request": request})
 
 
 @router.get("/analyzer", response_class=HTMLResponse)
 async def start(request: Request):
-    return templates.TemplateResponse("analyzer.html", {"request": request})
+    return Analyzer(context={"request": request})
--- app/main_service/functions/pdf.py
+++ app/main_service/functions/pdf.py
@@ -1,15 +1,20 @@
 import re
 
 import PyPDF2
+from fastapi import HTTPException
+from PyPDF2.errors import PdfReadError
 
 
 def parse_pdf(pdf_file) -> tuple[str, str, str]:
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    try:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+    except PdfReadError:
+        raise HTTPException(status_code=404, detail="Oops.. wrong file format")
+    
     text = ""
     for page in range(len(pdf_reader.pages)):
         text += pdf_reader.pages[page].extract_text()
 
-        # Extract name and email
     name: str = re.findall(r"([A-Z][a-z]+ [A-Z][a-z]+)", text)[0]
     email: str = re.findall(r"[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+", text)[0]
 
