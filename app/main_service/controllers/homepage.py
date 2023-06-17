bc402cfa11b6b35fd55694a9368911fad654df7f
HA-22L <112653868+HA-22L@users.noreply.github.com>
2023-06-17 18:04:17 +0300

    Add contact us page (#2)

--- app/main_service/controllers/homepage.py
+++ app/main_service/controllers/homepage.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
-from ..templates import About, Analyzer, Homepage
+from ..templates import About, Analyzer, ContactUs, Homepage
 
 router = APIRouter()
 
@@ -11,11 +11,16 @@ async def homepage(request: Request):
     return Homepage(context={"request": request})
 
 
+@router.get("/analyzer", response_class=HTMLResponse)
+async def start(request: Request):
+    return Analyzer(context={"request": request})
+
+
 @router.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
     return About(context={"request": request})
 
 
-@router.get("/analyzer", response_class=HTMLResponse)
-async def start(request: Request):
-    return Analyzer(context={"request": request})
+@router.get("/contact-us", response_class=HTMLResponse)
+async def contact_us(request: Request):
+    return ContactUs(context={"request": request})
