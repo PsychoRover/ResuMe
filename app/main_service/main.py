fe7c321c32464d0fd71093b5c161a946d2965fb1
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2023-06-17 12:00:47 +0000

    Typos And Misspells Fixes

--- app/main_service/main.py
+++ app/main_service/main.py
@@ -5,7 +5,7 @@ from app.main_service.constants import Folders
 from app.main_service.controllers import homepage, prediction
 
 # ------- App Configurations ------- #
-app = FastAPI(debug=True, tilte="ResuMe")
+app = FastAPI(debug=True, title="ResuMe")
 app.include_router(prediction.router)
 app.include_router(homepage.router)
 app.mount("/static", StaticFiles(directory=Folders.STATIC), name="static")
